7adcc28c70e9c8d919adc29ffdff2db93fea952b
monadic <alexis.richardson@gmail.com>
2026-04-09 18:52:06 +0100

    Bump cub-scan formula to 0.5.1

--- cub-scan.rb
+++ cub-scan.rb
@@ -4,26 +4,26 @@
 class CubScan < Formula
   desc "Scan Kubernetes and GitOps configuration for risk"
   homepage "https://confighub.com"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.0/confighub-scan-darwin-arm64.tar.gz"
-      sha256 "4c6086140b6628b9149d31fa18e77762958a7ca5b534f08fa9f03d3a24ccfc2e"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-darwin-arm64.tar.gz"
+      sha256 "ec55e72d94fe16c02db1e4d5a27e2fe4915f7cd5dd667ed5c276612279f2149a"
     else
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.0/confighub-scan-darwin-amd64.tar.gz"
-      sha256 "d6e5acd6bb51d6c3d3e2b2b32b901cda60a388d0d414942b42a62eec5505ba49"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-darwin-amd64.tar.gz"
+      sha256 "d088f0313d3ad5e38980015c9681257a6583f2b746fe35dde1857b21c9a66ecf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.0/confighub-scan-linux-arm64.tar.gz"
-      sha256 "66ec1f8c4dab37fcd7262a5240883dd9f343538f3582c57674f656ce79ef94e1"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-linux-arm64.tar.gz"
+      sha256 "a96d621898b1af939cb5e9eff5f96e08fba76e13be2ca1f8b15f9470e1669dff"
     else
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.0/confighub-scan-linux-amd64.tar.gz"
-      sha256 "b8217ccdf28a0f80256d46f296b91bd4a2aa92ee67c2ef7942175a9aebdc999d"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-linux-amd64.tar.gz"
+      sha256 "16e8664c2c67cc188ce4bab73053595105ed18a6671eeb2d762679e32fa0fb1f"
     end
   end
 
