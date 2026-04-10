69f85d60600c356abe1dbb723906fd5669ffa7f9
monadic <alexis.richardson@gmail.com>
2026-04-10 20:24:09 +0100

    Bump cub-scan formula to 0.6.0

--- cub-scan.rb
+++ cub-scan.rb
@@ -4,26 +4,26 @@
 class CubScan < Formula
   desc "Scan Kubernetes and GitOps configuration for risk"
   homepage "https://confighub.com"
-  version "0.5.1"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-darwin-arm64.tar.gz"
-      sha256 "ec55e72d94fe16c02db1e4d5a27e2fe4915f7cd5dd667ed5c276612279f2149a"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.6.0/confighub-scan-darwin-arm64.tar.gz"
+      sha256 "6eebda85b68cb461ea9a7c2b76d1b59aad6d62fb5d6d640114c92eb99a61c1b6"
     else
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-darwin-amd64.tar.gz"
-      sha256 "d088f0313d3ad5e38980015c9681257a6583f2b746fe35dde1857b21c9a66ecf"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.6.0/confighub-scan-darwin-amd64.tar.gz"
+      sha256 "680168543c97a23eb10df14ddb7413f632b06100300ca8c7f346317a4da45707"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-linux-arm64.tar.gz"
-      sha256 "a96d621898b1af939cb5e9eff5f96e08fba76e13be2ca1f8b15f9470e1669dff"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.6.0/confighub-scan-linux-arm64.tar.gz"
+      sha256 "1ecfef94eee8783f1cdb7f014487d1f244df2b4022320fba21973b0f2fc8719c"
     else
-      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.5.1/confighub-scan-linux-amd64.tar.gz"
-      sha256 "16e8664c2c67cc188ce4bab73053595105ed18a6671eeb2d762679e32fa0fb1f"
+      url "https://github.com/confighub/homebrew-tap/releases/download/cub-scan-v0.6.0/confighub-scan-linux-amd64.tar.gz"
+      sha256 "99bbce1d80ea4fa390b58bf41c13473a7af5a2f40672aeada1fed15def50849b"
     end
   end
 
@@ -44,6 +44,6 @@ class CubScan < Formula
 
   test do
     output = shell_output("#{bin}/cub-scan -h 2>&1")
-    assert_match "Path to normalized risk catalog", output
+    assert_match "cub-scan --capabilities", output
   end
 end
